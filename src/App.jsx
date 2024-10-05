7abf67be9414008b9840dd012cebdedbe63c581d
aashanamir <emailstormfacts@gmail.com>
2024-10-05 19:40:47 +0500

    Hero Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import HeroSection from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <HeroSection/>
     </div>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './style.module.css';
+
+const HeroSection = () => {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.content}>
+        <h1>Welcome To Braxas International</h1>
+        <p>
+        At Braxas International, we provide dedicated support to Yango captains—whether you drive a bike, car, or rickshaw. From seamless registration to 24/7 technical assistance, we ensure a smooth and profitable journey for all captains. Join us today and experience the best captain support services in the city.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.primaryButton}>Get Started</button>
+          <button className={styles.secondaryButton}>Learn More</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
