9dc951f80c1dbd0c269d92e28d4aea2c16e2310d
aashanamir <emailstormfacts@gmail.com>
2024-10-10 17:12:02 +0500

    Services Page Completed

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -3,13 +3,14 @@ import Navbar from "../../components/Navbar/Navbar";
 import HeroSection from '../../components/Hero/Hero';
 import Image from "../../assets/services.jfif";
 import Footer from '../../components/Footer/Footer';
-
+import Commitments from '../../components/commitments/Commitments';
 
 const Services = () => {
   return (
     <div>
       <Navbar />
       <HeroSection image={Image} heading={"Why Register With Braxas International"} desc={"Team up with Yango by partnering with your own transportation company or exploring investment opportunities. We've got your back every step of the way, from recruiting new drivers to scoring great deals on vehicles. Plus, we'll be there to help you manage things smoothly from day one"}/>
+      <Commitments />
       <Footer />
     </div>
   )
